f3ab21bbf254a3e118004ed0ecb64a0ac39d98fa
gavri1990 <giorgosgav1990@outlook.com.gr>
2025-08-30 17:15:40 +0300

    Added LazyColumn and BasicAlertDialog

--- src/main/java/com/example/shoppinglist/MainActivity.kt
+++ src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -4,26 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
+import com.example.shoppinglist.ui.theme.home.ShoppingList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,52 +19,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShoppingListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ShoppingList(modifier = Modifier
                         .fillMaxSize()
-                        .padding(innerPadding))
+                        .padding(innerPadding)
+                        .statusBarsPadding())
                 }
             }
         }
     }
 }
 
-@Composable
-fun ShoppingList(modifier: Modifier = Modifier){
-    val customTextStyle = TextStyle(color = Color.White, fontSize = 16.sp, fontFamily = FontFamily.Monospace)
-    val itemsButtonText = "Edit items"
-    Column(
-        modifier = Modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Box {
-                Button(onClick = {}) {
-                    Text(
-                        itemsButtonText,
-                        fontSize = 16.sp,
-                        fontStyle = FontStyle.Normal,
-                        style = customTextStyle
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ShoppingListPreview(){
-    ShoppingListTheme {
-        ShoppingList()
-    }
-}
